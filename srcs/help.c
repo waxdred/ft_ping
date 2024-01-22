596deb37f560d7d005057c6d2d4f846bba32dbc3
waxcoin <waxcoin@hotmail.com>
2024-01-22 18:59:31 +0100

    Update -h

--- srcs/help.c
+++ srcs/help.c
@@ -1,42 +1,23 @@
 #include "../includes/ft_ping.h"
 
-static void help1() {
-  fprintf(
-      stderr,
-      "ft_ping [-h help] [-c count] [-t ttl] [-v verbose] [-W timeout] [-w ");
-  fprintf(stderr, "deadline]");
-  fprintf(stderr, "[-w deadline] [-q silence]\n\n");
-  fprintf(stderr, "OPTIONS\n\n\t");
-  fprintf(stderr, "-c count\n\t\tS'arrêter après l'envoi de nombre paquets ");
-  fprintf(stderr, "ECHO_REQUEST.\n\t\t");
-  fprintf(stderr, "Combiné");
-  fprintf(stderr,
-          "à l'option  -w deadline,  ping  doit recevoir ses nombre paquets ");
-  fprintf(stderr, "ECHO_REPLY\n\t\t");
-  fprintf(stderr, "avant que la temporisation n'expire.\n\n\t");
-  fprintf(stderr, "-t ttl\n\t\tSpécifie le champ IP « Time to Live ».\n\n\t");
-  fprintf(stderr, "-v verbose\n\t\tSortie verbeuse.\n\n\t");
-  fprintf(
-      stderr,
-      "-W deadline \n\t\tFixe un délai (en secondes),  avant que  ping ne ");
-}
-
 void help(char *s) {
-  fprintf(stderr, "Usage: sudo %s ip_adress\n\n", s);
-  help1();
-  fprintf(stderr, "mette fin à son");
-  fprintf(stderr, "reçus.Note ");
-  fprintf(stderr, ": ici,");
-  fprintf(stderr,
-          "ping ne s 'arrêtera pas  après l' envoi de quelques paquets,\n\t\t");
+  fprintf(stderr, "Usage: sudo %s [OPTIONS] ip_adress\n\n", s);
+  fprintf(stderr, "Options:\n");
+  fprintf(stderr, "  -h\t\tAfficher cette aide et quitter.\n");
   fprintf(
       stderr,
-      "mais attendra que le délai expire ou que nombre sondes aient reçu une");
+      "  -c count\tNombre de paquets ECHO_REQUEST à envoyer (puis arrêter).\n");
   fprintf(stderr,
-          "ou encore qu'une notification d'erreur provienne du réseau. \n\n\t");
-  fprintf(stderr, "\n\n\t -q silence \n\t\tSortie silencieuse. Rien n'est "
-                  "affiché sauf les ");
-  fprintf(stderr,
-          "lignes \n\t\t de résumé au démarrage et à la fin de l'exécution. ");
-  fprintf(stderr, "?)\n\n\t");
+          "  -t ttl\tDéfinir la valeur TTL (Time to Live) des paquets IP.\n");
+  fprintf(stderr, "  -v\t\tActiver le mode verbeux (affiche des informations "
+                  "supplémentaires).\n");
+  fprintf(stderr, "  -W timeout\tSpécifier un délai maximal (en secondes) pour "
+                  "attendre une réponse.\n");
+  fprintf(stderr, "  -w deadline\tDéfinir une échéance (en secondes) après "
+                  "laquelle ft_ping s'arrête,\n");
+  fprintf(
+      stderr,
+      "         \tpeu importe combien de paquets ont été envoyés ou reçus.\n");
+  fprintf(stderr, "  -q\t\tMode silencieux. Affiche uniquement les résumés de "
+                  "début et de fin.\n");
 }
