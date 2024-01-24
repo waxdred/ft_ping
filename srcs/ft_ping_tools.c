9ef3c97fa799384c7603acc3f8857f84aa21321e
waxcoin <waxcoin@hotmail.com>
2024-01-24 11:04:59 +0100

    Add id for icmp receive

--- srcs/ft_ping_tools.c
+++ srcs/ft_ping_tools.c
@@ -112,11 +112,17 @@ int ft_receive(t_ping *ping, struct timeval dev) {
   }
 #ifdef __OS__
   struct icmp *ipcmp = (struct icmp *)(r.buf + sizeof(struct ip));
+  unsigned int id = ntohs(ipcmp->icmp_id);
 #else
   struct icmphdr *icmp = (struct icmphdr *)(r.buf + sizeof(struct iphdr));
+  unsigned int id = ntohs(icmp->un.echo.id);
 #endif
   gettimeofday(&r.end, NULL);
   ft_cmp_address(ping, &r);
+  if (ping->flag.verbose.value == 0) {
+    printf("id 0x%x = %d\n", id, id);
+    ping->flag.verbose.value = 1;
+  }
   switch (ipcmp->icmp_type) {
   case ICMP_ECHO:
     break;
